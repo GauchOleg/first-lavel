98df208d4c5123c270271f7e484e390a9d47fa07
Oleg <gauch_oleg@ukr.net>
2017-07-12 17:21:41 +0300

    example

---
+++ test.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12.07.2017
+ * Time: 16:22
+ */
+
+//$name = "Олег";
+//$age = 28;
+//echo "Меня завут $name мне $age лет";
+//unset($age);
+
+//if (!defined(OLEG)){
+//    define('OLEG', OLEG);
+//}else{
+//    echo "This const exist!";
+//}
+//echo OLEG;
+
+//$output ='';
+//$age = 65;
+//
+//if ($age >= 18 && $age <= 59){
+//    $output = "Вам еще работать и работать..";
+//}elseif ($age > 59){
+//    $output = "Вам пора на пенсию";
+//}elseif ($age >= 1 && $age <= 17){
+//    $output = 'Вам еще рано работать';
+//}else{
+//    $output = 'Неизвестный возраст';
+//}
+//echo $output;
+
+//$day = 1;
+//switch ($day){
+//    case '1':
+//    case '2':
+//    case '3':
+//    case '4':
+//    case '5':
+//        $output .= 'Это рабочий день';
+//        break;
+//    case '6':
+//    case '7':
+//        $output .= 'Это выходной день';
+//        break;
+//    default :
+//        $output = 'Не вырный формат дня недели';
+//
+//}
+//echo $output;
+
+//$bmw = array(
+//    "model" => "x5",
+//    "speed" => 120,
+//    "doors" => 5,
+//    "year" => 2006
+//);
+//$toyota = array(
+//    "model" => "Carina",
+//    "speed" => 130,
+//    "doors" => 4,
+//    "year" => 2007,
+//);
+//$opel = array(
+//    "model" => "Corsa",
+//    "speed" => 140,
+//    "doors" => 5,
+//    "year" => 2007,
+//);
+//echo "<p>BMW - $bmw[model] - $bmw[speed] - $bmw[doors] - $bmw[year]";
+//echo "<p>Toyota - $toyota[model] - $toyota[speed] - $toyota[doors] - $toyota[year]";
+//echo "<p>Opel - $opel[model] - $opel[speed] - $opel[doors] - $opel[year]";
+
+//for ($i = 1; $i < 50; $i+=2){
+//    echo '<p>' . $i;
+//}
+
+$cols = 10;
+$rows = 10;
+$color = " style='background: red'";
+
+//echo "<table border='1'>";
+//for ($tr = 1; $tr <= $rows; $tr++){
+//    echo '<tr>';
+//    for ($td = 1; $td <= $cols; $td++){
+//        if ($tr == 1 || $td == 1){
+//            echo "<th$color>" . $td * $tr .'</th>';
+//        }else {
+//            echo '<td>' . $td * $tr .'</td>';
+//        }
+//    }
+//    echo '</tr>';
+//}
+//echo '</table>';
